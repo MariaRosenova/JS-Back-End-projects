a22b00d0522859ddb437cfec9ea9bbe422b4a0fe
Maria <mimito1353@gmail.com>
2024-01-26 11:01:23 +0200

    Add 404 page

--- movie-magic/src/index.js
+++ movie-magic/src/index.js
@@ -13,9 +13,8 @@ app.engine('hbs', handlebars.engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.use(routes);
-
 app.use(express.static(path.join(__dirname,'public')));
 
+app.use(routes);
 
 app.listen(port, () => console.log(`The app is running on port ${port}...`))
