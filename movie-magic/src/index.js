8b8d2d86e8c1b501f862a10d6b59c17b4cb32fd3
Maria <mimito1353@gmail.com>
2024-01-26 09:57:21 +0200

    Add about page

--- movie-magic/src/index.js
+++ movie-magic/src/index.js
@@ -11,7 +11,7 @@ app.engine('hbs', handlebars.engine({
     extname: 'hbs',
 }));
 app.set('view engine', 'hbs');
-app.set('home', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views'));
 
 app.use(routes);
 
