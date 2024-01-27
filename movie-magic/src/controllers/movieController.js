3d38d9f0eb24cf60c950281121d47bd86904c2f4
Maria <mimito1353@gmail.com>
2024-01-27 18:26:01 +0200

    Add rating

--- movie-magic/src/controllers/movieController.js
+++ movie-magic/src/controllers/movieController.js
@@ -15,6 +15,8 @@ router.get('/movies/:movieId', (req, res) => {
     const movieId = req.params.movieId;
     const movie = movieService.getOne(movieId);
     console.log(movie)
+
+    movie.rating = new Array(movie.rating).fill(true);
     res.render('details', {movie});
 });
 
