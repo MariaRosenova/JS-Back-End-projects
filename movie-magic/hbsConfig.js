01478075e5c83f41f5708208eaf5a7cdc9e673c5
Maria <mimito1353@gmail.com>
2024-02-04 09:45:45 +0200

    removed resources

--- movie-magic/hbsConfig.js
+++
@@ -1,9 +0,0 @@
-const handlebars = require('express-handlebars');
-const path = require('path');
-
-function hbsConfig(app) {
-    app.engine('handlebars', handlebars.engine());
-    app.set('view engine', 'handlebars')
-}
-
-module.exports = hbsConfig;
