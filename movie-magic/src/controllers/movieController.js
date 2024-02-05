1b566e375b0c172fedebf8771017c9701b61113e
Maria <mimito1353@gmail.com>
2024-02-05 10:18:11 +0200

    Show cast on details page

--- movie-magic/src/controllers/movieController.js
+++ movie-magic/src/controllers/movieController.js
@@ -25,7 +25,10 @@ router.post('/create', async (req, res) => {
 
 router.get('/movies/:movieId', async (req, res) => {
     const movieId = req.params.movieId;
+    
     const movie = await movieService.getOne(movieId).lean();
+    //const casts = await castService.getByIds(movie.casts).lean();
+
     
     movie.rating = new Array(Number(movie.rating)).fill(true);
     
