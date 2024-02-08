bed064c1fbd344635831a9a7162b7f5aedf93744
Maria <mimito1353@gmail.com>
2024-02-08 14:32:59 +0200

    add 404 page

--- course-book/routes.js
+++ course-book/routes.js
@@ -8,4 +8,10 @@ router.use(homeController);
 router.use('/auth', authController);
 
 router.use(coursesActionsController);
+
+router.get('*', (req, res) => {
+    res.render('404');
+});
+
+
 module.exports = router;
