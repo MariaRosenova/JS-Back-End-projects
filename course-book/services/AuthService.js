701a3bf3a407b78047c23d9189bbb1ef0d47744e
Maria <mimito1353@gmail.com>
2024-02-09 14:31:52 +0200

    create a token

--- course-book/services/AuthService.js
+++ course-book/services/AuthService.js
@@ -1,6 +1,7 @@
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
 const mongoose = require("mongoose");
+const jsonwebtoken = require('jsonwebtoken')
 
 exports.createUser = (userData) => {
   const user = await.findOne({ email: userData.email });
@@ -25,4 +26,14 @@ exports.login = async (loginData) => {
   } else {
     console.log("is valid keep calm");
   }
+
+  const payload = {
+    username: user.username,
+  };
+
+  const SECRET = 'kddkfksklfo5fs5f8f2f75';
+
+  const token = jsonwebtoken.sign(payload, SECRET, {expiresIn: '2h'});
+
+  
 };
