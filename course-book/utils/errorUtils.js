2c22536767c924ab67616f96c88fa17570ce37bc
Maria <mimito1353@gmail.com>
2024-02-14 10:01:50 +0200

    edit error utils

--- course-book/utils/errorUtils.js
+++ course-book/utils/errorUtils.js
@@ -1,13 +1,11 @@
 const mongoose = require('mongoose');
 
 exports.getErrorMessage = (err) => {
-    let message = '';
-
+   
     if (err instanceof mongoose.MongooseError) {
-        message = Object.values(err.errors).at(0).message;
+        //throw new error
+        return Object.values(err.errors)[0].message;
     } else if (err instanceof Error) {
-        message = err.message;
+        return err.message;
     }
-
-    return message;
-}
+};
