10b89635eeef48210600e4a24a770657075fcd11
Maria <mimito1353@gmail.com>
2024-02-14 09:57:25 +0200

    Add error handling in authController

--- course-book/controllers/authController.js
+++ course-book/controllers/authController.js
@@ -1,28 +1,25 @@
 const router = require("express").Router();
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
 const authService = require("../services/AuthService");
-const { getErrorMessage } = require('../utils/errorUtils');
-
+const { getErrorMessage } = require("../utils/errorUtils");
 
 router.get("/login", (req, res) => {
- 
   res.render("auth/login");
 });
 
 router.post("/login", async (req, res) => {
-    const loginData = req.body;
+  const loginData = req.body;
 
-    try {
-      const token = await authService.login(loginData);
-     
-      res.cookie('auth', token);
-      res.redirect('/');
-  
-    } catch (err) {
-      const message = getErrorMessage(err);
-    
-      res.render('auth/login', {...loginData, error: message});
-    }
+  try {
+    const token = await authService.login(loginData);
+
+    res.cookie("auth", token);
+    res.redirect("/");
+  } catch (err) {
+    const message = getErrorMessage(err);
+
+    res.render("auth/login", { error: getErrorMessage(err) });
+  }
 });
 
 router.get("/register", (req, res) => {
@@ -32,22 +29,17 @@ router.get("/register", (req, res) => {
 router.post("/register", async (req, res) => {
   const registerData = req.body;
   try {
-
-      await authService.createUser(registerData);
-      res.redirect('/login')
-  } catch(err) {
-    const message = getErrorMessage(err);
+    await authService.register(registerData);
     
-    res.render('auth/register', {...registerData, error: message});
+  } catch (err) {
+    const message = getErrorMessage(err);
+      res.render("auth/register", { error: getErrorMessage(err) });
   }
-
-
 });
 
 router.get("/logout", (req, res) => {
-    res.clearCookie('auth');
-
-    res.redirect('/')
+  res.clearCookie("auth");
+  res.redirect("/");
 });
 
 module.exports = router;
